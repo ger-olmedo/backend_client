004e1a7f514aaa323af85af21a491f469ae6f3ea
ger-olmedo <ger.olmedo@hotmail.com>
2020-11-17 21:12:30 -0300

    repaso de lo gestionado en el backend

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require ('express')
 const app = express()
 app.listen(2000)
+app.use (express.static('public'))
 
-app.get('/' , (req, res) => {
-    res.end('Hola desde express.giJS')
+app.get('/:seccion' , (req, res) => {
+    const {seccion} = req.params
+
+    res.writeHead(200, {"Content-Type" : "text/html"})
+    res.end(`<h1>Hola desde la seccion <u>${seccion}</u> en HTML</h1>`)
 })
